15205bd258d0a0002093f84442e1e33c48f0c8b6
Champa Lal suthar <135296999+champalalsuthar@users.noreply.github.com>
2024-02-10 10:36:57 +0530

    Update db url change connection.js

--- database/connection.js
+++ database/connection.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const mongoose = require('mongoose')
 const DBURL = process.env.DB_CONNECTION;
 
-mongoose.connect(DBURL).then(()=>{
+mongoose.connect("mongodb+srv://champalaljangid9292:champa123@cluster0.iwftirj.mongodb.net/?retryWrites=true&w=majority").then(()=>{
     console.log("database connected")
 }).catch((err)=>{
     console.log(err)
